2b24fc3b2d0766ff16b10ce13a19263f63fbec03
suguru <suguru.a.nakano@rakuten-sec.co.jp>
2020-09-17 18:29:10 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<<<<<<< HEAD
+
+$integer = 20;
+echo $integer
+$string = "abc";
+echo $string;
+=======
+<?php
+
+$a=3;
+$b=7;
+echo $a+$b;
+
+$array = ['January','Feburary','March','April','May','June','July','August','September','October','November','December'];
+echo $array[7];
+
+
+$hello=" Hello, ";
+$name="Suguru";
+$world='‘s World!';
+echo $hello.$name.$world;
+
+
+$tech_boost=' tech';
+$tech_boost .=' boost';
+echo $tech_boost;
+
+
+$calendar_2020 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" =>"6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => '10月',
+  "November" => "11月",
+  "December" => "12月",
+];
+
+// 12月を表示する
+echo $calendar_2020["December"];
+
+
+>>>>>>> origin/master
